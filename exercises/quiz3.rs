76ca3b728433b55c24e33d8e2760f6a8d2e01636
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-12-03 22:22:24 +0530

    done upto 72

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -1,5 +1,3 @@
-// quiz3.rs
-//
 // This quiz tests:
 // - Generics
 // - Traits
@@ -16,18 +14,39 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// Add a trait to represent different types of grades
+trait Grade: std::fmt::Display {
+    fn display(&self) -> String;
+}
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            &self.grade // Directly use &self.grade here
+        )
+    }
+}
+
+// Implement Grade for f32 (numeric grades)
+impl Grade for f32 {
+    fn display(&self) -> String {
+        self.to_string()
+    }
+}
+
+// Implement Grade for String (alphabetical grades)
+impl Grade for String {
+    fn display(&self) -> String {
+        self.clone()
     }
 }
 
@@ -50,9 +69,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
